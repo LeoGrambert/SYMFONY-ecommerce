ef26251e682860f4331fdbe2cd7cc0b191e66c30
LeoGrambert <leogrambert@gmail.com>
2017-03-16 18:21:39 +0100

    Regex complexification on birthdate field on step two

--- Symfony/src/LG/CoreBundle/Resources/public/js/booking.js
+++ Symfony/src/LG/CoreBundle/Resources/public/js/booking.js
@@ -172,7 +172,7 @@ $(function($) {
                     var $country = false;
                     $('#form_'+$visitor+' .form-group .country').css('borderColor', '#ef5050');
                 }
-                if(!$birthDateValue.match(/^(\d{1,2})-(\d{1,2})-(\d{4})$/))
+                if(!$birthDateValue.match(/^(?:(?:31(-)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(-)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(-)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(-)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{4})$/))
                 {
                     isValid = false;
                     var $birthdate = false;
@@ -316,7 +316,7 @@ $(function($) {
    var onSuccessSubmit = function () {
         var $url = window.location.href;
         var $id = $url.split('2/');
-        window.location.replace('/front/projets/louvre/website/web/fr/booking/create/3/'+$id[1]);
+        window.location.replace('/front/projets/louvre/Symfony/web/fr/booking/create/3/'+$id[1]);
     };
 
     /**
